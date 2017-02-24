5c7e732bcfa71b5b0d750ba98f2d472fe1c90bf4
Paul van Schayck <polleke@gmail.com>
2017-02-24 18:10:58 +0100

    Initial version. Add elk and logspout

---
+++ rit.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+
+set -e
+
+
+externals=""
+
+if [[ $1 == "externals" ]]; then
+    mkdir -p externals
+
+    if [[ $2 == "clone" ]]; then
+        # Ignore error during cloning, as we don't care about existing dirs
+        set +e
+        while read -r external; do
+            external=($external)
+            echo -e "\e[32m =============== ${external[0]} ======================\033[0m"
+            git clone ${external[1]} ${external[0]}
+        done <<< "$externals"
+    fi
+
+    if [[ $2 == "status" ]]; then
+        while read -r external; do
+            external=($external)
+            echo -e "\e[32m =============== ${external[0]} ======================\033[0m"
+            git -C ${external[0]} status
+        done <<< "$externals"
+    fi
+
+    if [[ $2 == "pull" ]]; then
+        while read -r external; do
+            external=($external)
+            echo -e "\e[32m =============== ${external[0]} ======================\033[0m"
+            git -C ${external[0]} pull --rebase
+        done <<< "$externals"
+    fi
+    exit 0
+fi
+
+
+if [[ -z $RIT_ENV ]]; then
+    RIT_ENV="local"
+
+    if [[ $HOSTNAME == "fhml-srv018" ]]; then
+        RIT_ENV="tst"
+    fi
+
+    if [[ $HOSTNAME == "fhml-srv019" ]]; then
+        RIT_ENV="dev1"
+    fi
+
+    if [[ $HOSTNAME == "fhml-srv020" ]]; then
+        RIT_ENV="dev2"
+    fi
+fi
+export RIT_ENV
+
+# Create networks
+
+if ! docker network inspect corpus_default > /dev/null 2>&1; then
+   docker network create corpus_default
+fi
+if ! docker network inspect oculus_default > /dev/null 2>&1; then
+   docker network create oculus_default
+fi
+
+# Set the prefix for the project
+COMPOSE_PROJECT_NAME="common"
+export COMPOSE_PROJECT_NAME
+
+# Assuming docker-compose is available in the PATH
+docker-compose "$@"
